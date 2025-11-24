53bb0bec638ca3af00f26bc2ebeb35a0f47db76b
unknown <dtecdasilva@gmaile.com>
2025-11-24 14:49:34 +0100

    responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Brico",
   description: "The best real estate website in Cameroon",
+  icons: {
+    icon: "/logo.png"
+  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
